e7d4db15565800142ecb85e3adc3af820b66651b
Safee <sh3723@ic.ac.uk>
2026-01-31 19:39:11 +0000

    manage to get the simulation to be working using numpy arrays and dataframe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import PySide6.QtCore
+# Instead of writing using a dataframe, perhaps write using np arrays
 
 # Importing functions
 from math import sqrt
@@ -18,70 +18,54 @@ print(Radius)
 mu = 3.986e14
 
 # time settings
-t0 = 0
-tf = 8000
-dt = 0.1
+t0 = 0.0
+tf = 8000.0
+dt = 1
+t = np.arange(t0, tf, dt)
 n = int(tf // dt)
 
+# Function for finding the next time instance
+def x1(x0,  mu):
+    r = sqrt(x0[0] ** 2 + x0[1] ** 2)
+    acc = -mu / (r**3) * np.array([x0[0], x0[1]])
+    x1 = np.array([x0[2], x0[3], acc[0], acc[1]]) * dt + x0
+    return x1
 
-# State space, 2D satellite
-
-# Using single lists
-# x, u, y, v
-# x = [Radius]
-# y = [0.0]
-# u = [0.0]
-# v = [10000.0]
-
-# Using Data Frames
-# x0 = pd.DataFrame(
-#     {
-#         "x": [Radius],
-#         "y": [0.0],
-#         "u": [0.0],
-#         "v": [1000.0],
-#     }
-# )
-
-cols = ["x", "y", "u", "v"]
-x0 = pd.DataFrame(np.nan, index=range(n+1), columns=cols)
-
-x0.loc[0] = [Radius, 0.0, 0.0, 1000.0]  # set initial state
+# State space, 2D satellite, using an array
+# x0 = [x, y, u, v]
+v0 = np.sqrt(mu / Radius)
+x0 = np.array([Radius, 0.0, 0.0, v0])
+print(x0)
 
 
-print(x0.iloc[0, 0])
+# File header writing
+with open("history.txt", "w") as f:
+    f.write("t, x, y, u, v\n")          # optional separator/header line
+# File array writing
+with open("history.txt", "a") as f:
+    for i in range(n):
+        t_write = str(t[i])
+        print(t_write)
+        f.write(t_write)
+        f.write(", ")
+        np.savetxt(f, x0.reshape(1, -1), fmt="%g", delimiter=",")
+        x0 = x1(x0, mu)
 
 
-# I always keep forgetting the syntax for numpy
-# print(x0[0, 0])
-# print(x0[1, 0])
 
-# Same goes for matplotlib
-fig, ax = plt.subplots(figsize=(7, 7), dpi=200)
+# Plotting
+df = pd.read_csv("history.txt")
+df.columns = df.columns.str.strip()
+print(df)
+
+time = df['t'].to_numpy()
+
+x = df['x'].to_numpy()
+y = df['y'].to_numpy()
+u = df['u'].to_numpy()
+v = df['v'].to_numpy()
 
 
-# r = sqrt(x[0]**2 + y[0]**2)
-r = sqrt(x0.iloc[0, 0] ** 2 + x0.iloc[0, 0] ** 2)
-
-for i in range(0, n):
-    # acc_x = -mu * x[i] / (r**3)
-    # acc_y = -mu * y[i] / (r**3)
-    acc = -mu / (r**3) * np.array([x0.iloc[i, 0], x0.iloc[i, 1]])
-
-    # Distance changes
-    # x.append(u[i]*dt + x[i])
-    # y.append(v[i]*dt + y[i])
-    x0.iloc[i + 1, 0] = x0.iloc[i, 2] * dt + x0.iloc[i, 0]
-    x0.iloc[i + 1, 1] = x0.iloc[i, 3] * dt + x0.iloc[i, 1]
-
-    # Velocity changes
-    # u.append(u[i] + acc_x*dt)
-    # v.append(v[i] + acc_y*dt)
-    x0.iloc[i + 1, 2] = acc[0] * dt + x0.iloc[i, 2]
-    x0.iloc[i + 1, 3] = acc[1] * dt + x0.iloc[i, 3]
-
-# print("x = " , x , "y = " , y)
-
-# print(x0)
-plt.plot(x0.x, x0.y)
+fig, ax = plt.subplots(figsize=(2, 2), dpi=500)
+plt.plot(x, y)
 plt.show()
